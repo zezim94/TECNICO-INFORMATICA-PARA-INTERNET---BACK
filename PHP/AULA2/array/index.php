8dda2dba10fb99bec24feab566c40309e280c1f9
Andelson <andelsonascimento@gmail.com>
2026-02-04 16:21:10 -0300

    array

--- PHP/AULA2/array/index.php
+++ PHP/AULA2/array/index.php
@@ -9,6 +9,37 @@
 
 <body>
 
+    <?php
+    $array = array("nome" => "Andelson", "idade" => 31, "cidade" => "Cubatão");
+    print_r($array);
+    echo "<br>";
+
+    foreach ($array as $key => $value) {
+        echo $key . " : " . $value . "<br>";
+    }
+
+    echo "<hr>";
+
+    $cidade = ["Cubatão", "São Paulo", "Rio de Janeiro", "Santos", "Guarulhos"];
+
+    print_r($cidade);
+
+    echo "<br>";
+
+    foreach ($cidade as $value) {
+        echo $value . "<br>";
+    }
+
+    echo "<hr>";
+
+    $animais = ["gato", "cachorro", "pássaro", "pescoco", "coelho", "pato", "girafa", "porco", "ovelha", "abelha"];
+    echo "Animais: <br>";
+    foreach ($animais as $animal) {
+        echo $animal  . "<br>";
+    }
+
+    ?>
+
 </body>
 
 </html>
