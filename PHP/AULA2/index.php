38b63f435547d7ee6a3af9706b3e319fb950ad7b
Andelson <andelsonascimento@gmail.com>
2026-02-04 13:51:09 -0300

    imc

---
+++ PHP/AULA2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMC</title>
+</head>
+
+<body>
+
+    <form action="processa.php" method="post">
+        <label for="peso">Peso:</label>
+        <input type="number" name="peso" id="peso" placeholder="Digite seu peso">
+        <br>
+        <label for="altura">Altura:</label>
+        <input type="number" name="altura" id="altura" placeholder="Digite sua altura">
+        <br>
+        <input type="submit" value="Calcular">
+    </form>
+
+</body>
+
+</html>
