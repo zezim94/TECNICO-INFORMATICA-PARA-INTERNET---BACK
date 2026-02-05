054b6be6d1137a10dfa05b75e52bde9698724d69
Andelson <andelsonascimento@gmail.com>
2026-02-05 13:43:58 -0300

    fim

--- PHP/AULA2/desafio/desafio.php
+++ PHP/AULA2/desafio/desafio.php
@@ -9,13 +9,51 @@
 
 <body>
 
-<h1>Desáfio</h1>
+    <h1>Desáfio</h1>
 
-<?php
+    <?php
+
+    $numeros = [10, 5, 7, 8, 9];
+
+    foreach ($numeros as $num) {
+        if ($num === 7) {
+            echo "Encontrei o número 7";
+            break;
+        }
+    }
+
+
+    echo "<br>";
+    echo "<hr>";
+
+    $sete = $numeros[2];
+    $nove = $numeros[4];
+
+    echo $sete * $nove;
+
+    echo "<br>";
 
 
 
-?>
+    $sorteio = rand(1, 10);
+
+    if (!in_array($sorteio, $numeros)) {
+        $numeros[] = $sorteio;
+        echo "O número $sorteio foi adicionado na array";
+    }
+    else{
+        echo "O número $sorteio já existe na array";
+    }
+
+    echo "<hr>";
+
+    foreach ($numeros as $num) {
+
+        echo $num;
+        echo "<br>";
+    }
+
+    ?>
 
 </body>
 
