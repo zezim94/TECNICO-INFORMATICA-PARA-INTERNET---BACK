987a5390a4fcd647d95c3bf37e2e490b936a2a3f
Andelson <andelsonascimento@gmail.com>
2026-02-11 16:09:49 -0300

    fim uc10

--- UC10/index.php
+++ UC10/index.php
@@ -1,40 +1,101 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Cinema</title>
 </head>
 
 <body>
-    <h1>Avatar: Fogo e Cinzas</h1>
 
-    <form action="processa.php" method="POST">
+    <div class="container">
 
-        <label for="name">Nome:</label>
-        <input type="text" id="name" name="name" required>
-        <br><br>
-        <label for="idade">Idade:</label>
-        <input type="number" id="idade" name="idade" required>
-        <br><br>
-        <label for="sexo">Sexo:</label>
-        <input type="radio" id="sexo" name="sexo" value="Masculino">
-        <label for="sexo">Masculino</label>
-        <input type="radio" id="sexo" name="sexo" value="Feminino">
-        <label for="sexo">Feminino</label>
-        <br><br>
-        <label for="nota">O que você achou do filme?</label>
-        <select name="nota" id="nota">
-            <option value="1">Excelente</option>
-            <option value="2">Bom</option>
-            <option value="3">Regular</option>
-            <option value="4">Péssimo</option>
-        </select>
-        <br><br>
-        <input type="submit" value="Enviar">
-    </form>
 
+        <h1>Avatar: Fogo e Cinzas</h1>
+
+        <form action="processa.php" method="POST">
+
+            <div class="group">
+                <label for="name">Nome:</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+
+            <div class="group">
+                <label for="idade">Idade:</label>
+                <input type="number" id="idade" name="idade" required>
+            </div>
+
+            <div class="group">
+
+                <label>Sexo:</label>
+                <div class="sexo">
+                    <input type="radio" id="sexom" name="sexo" value="Masculino">
+                    <label for="sexom">Masculino</label>
+                    <input type="radio" id="sexof" name="sexo" value="Feminino">
+                    <label for="sexof">Feminino</label>
+                </div>
+            </div>
+
+            <div class="group">
+                <label for="nota">O que você achou do filme?</label>
+                <select name="nota" id="nota">
+                    <option value="excelente">Excelente</option>
+                    <option value="bom">Bom</option>
+                    <option value="regular">Regular</option>
+                    <option value="pessimo">Péssimo</option>
+                </select>
+            </div>
+            <input type="submit" value="Enviar">
+        </form>
+
+
+
+        <div class="resultado">
+
+            <?php
+
+
+            if (isset($_SESSION['dados'])) { ?>
+
+                <h2>Resultado</h2>
+
+                <h3>Total de Pessoas: </h3>
+                <p><?php echo $_SESSION['totalPessoas']; ?></p>
+                <h3>Total Idade:</h3>
+                <p><?php echo $_SESSION['totalIdade']; ?></p>
+                <h3>Média Idade:</h3>
+                <p><?php echo $_SESSION['mediaIdade']; ?></p>
+
+                <h3>Total Homens:</h3>
+                <p><?php echo $_SESSION['totalMas']; ?></p>
+                <h3>Total Mulheres:</h3>
+                <p><?php echo $_SESSION['totalFem']; ?></p>
+                <h3>Masculino:</h3>
+                <p><?php echo $_SESSION['mas']; ?>%</p>
+                <h3>Feminino:</h3>
+                <p><?php echo $_SESSION['fem']; ?>%</p>
+
+                <h3>Excelente:</h3>
+                <p><?php echo $_SESSION['excelente']; ?>%</p>
+                <h3>Bom:</h3>
+                <p><?php echo $_SESSION['bom']; ?>%</p>
+                <h3>Regular:</h3>
+                <p><?php echo $_SESSION['regular']; ?>%</p>
+                <h3>Péssimo:</h3>
+                <p><?php echo $_SESSION['pessimo']; ?>%</p>
+
+            <?php
+            }
+            ?>
+        </div>
+    </div>
 
 </body>
 
--- UC10/processa.php
+++ UC10/processa.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -7,15 +7,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sexo = $_POST["sexo"];
     $nota = $_POST["nota"];
 
-    $dados[] = array($name, $idade, $sexo, $nota);
+
 
     $total_pessoas = 0;
     $total_idade = 0;
+    $total_homens = 0;
+    $total_mulheres = 0;
+    $total_excelentes = 0;
+    $total_bons = 0;
+    $total_regulares = 0;
+    $total_pesimos = 0;
 
-    foreach ($dados as $dado) {
+    $_SESSION['dados'][] = [
+        'nome' => $name,
+        'idade' => $idade,
+        'sexo' => $sexo,
+        'nota' => $nota
+    ];
+
+    foreach ($_SESSION['dados'] as $dado) {
         $total_pessoas++;
 
-        $total_idade += $idade;
+        $total_idade += $dado['idade'];
 
 
         if ($sexo == "Masculino") {
@@ -35,15 +48,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    $mas = $total_homens / $total_pessoas * 100;
-    $fem = $total_mulheres / $total_pessoas * 100;
+    $_SESSION['totalPessoas'] = count($_SESSION['dados']);
+    $_SESSION['totalMas'] = $total_homens;
+    $_SESSION['totalFem'] = $total_mulheres;
+    $_SESSION['mas'] = number_format($total_homens / $total_pessoas * 100, 2);
+    $_SESSION['fem'] = number_format($total_mulheres / $total_pessoas * 100, 2);
 
-    $excelente = $total_excelentes / $total_pessoas * 100;
-    $bom = $total_bons / $total_pessoas * 100;
-    $regular = $total_regulares / $total_pessoas * 100;
-    $pessimo = $total_pesimos / $total_pessoas * 100;
+    $_SESSION['excelente'] = number_format($total_excelentes / $total_pessoas * 100, 2);
+    $_SESSION['bom'] = number_format($total_bons / $total_pessoas * 100, 2);
+    $_SESSION['regular'] = number_format($total_regulares / $total_pessoas * 100, 2);
+    $_SESSION['pessimo'] = number_format($total_pesimos / $total_pessoas * 100, 2);
 
-    $media_idade = $total_idade / $total_pessoas;
-}else{
+    $_SESSION['totalIdade'] = $total_idade / $total_pessoas;
+    $_SESSION['mediaIdade'] = number_format($total_idade / $total_pessoas, 2);
+
+    header('Location: index.php');
+} else {
     header("Location: index.php");
 }
