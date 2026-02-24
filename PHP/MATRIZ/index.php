cbd7ee199a880efbe301518927d0c3d1eaa2d9ad
Andelson <andelsonascimento@gmail.com>
2026-02-24 15:26:46 -0300

    fim

---
+++ PHP/MATRIZ/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matriz</title>
+</head>
+
+<body>
+
+    <h1>Matriz</h1>
+
+</body>
+
+</html>
