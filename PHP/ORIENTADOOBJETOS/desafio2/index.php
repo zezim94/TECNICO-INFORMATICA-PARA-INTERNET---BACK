984e3f6f0ab082368fe98a845c8dfdbad631c387
Andelson <andelsonascimento@gmail.com>
2026-04-13 16:25:35 -0300

    fim encapsulamento

--- PHP/ORIENTADOOBJETOS/desafio2/index.php
+++ PHP/ORIENTADOOBJETOS/desafio2/index.php
@@ -1,17 +1,105 @@
+<style>
+    body{
+        background-color: #0284fdff;
+    }
+</style>
+
 <?php
 
 abstract class Times
 {
     public $nome, $titulos, $local, $tecnico;
+
+    public function __construct($nome, $titulos, $local, $tecnico)
+    {
+        $this->nome = $nome;
+        $this->titulos = $titulos;
+        $this->local = $local;
+        $this->tecnico = $tecnico;
+    }
+
+    public function hino($letra)
+    {
+        echo $letra;
+    }
+
+    public function bandeira($bandeira)
+    {
+        echo $bandeira;
+    }
+
+    public function __set($propriedade, $valor)
+    {
+        $this->$propriedade = $valor;
+    }
+
+    public function __get($propriedade)
+    {
+        return $this->$propriedade;
+    }
 }
 
-class Selecao {
-    
+class Selecao extends Times
+{
+    public $sede, $continente;
 }
 
-class Clubes
+class Clubes extends Times
 {
     public $divisao, $rebaixado;
 }
 
-class ClubesAmadores {}
+class ClubesAmadores extends Clubes
+{
+    public $patrocinador, $categoria;
+}
+
+$brasil = new Selecao("Brasil", 5, "América do Sul", "Carlo");
+$brasil->sede = "2 vezes";
+$brasil->continente = "América do Sul";
+
+echo "Seleção: " . $brasil->nome;
+echo "<br>";
+echo " continente: " . $brasil->continente;
+echo "<br>";
+echo " titulos: " . $brasil->titulos;
+echo "<br>";
+echo " local: " . $brasil->local;
+echo "<br>";
+echo " tecnico: " . $brasil->tecnico;
+echo "<br>";
+echo " já foi sede: " . $brasil->sede;
+echo "<br>";
+"Hino: " .$brasil->hino("Ouviram do Ipiranga as margens plácidas
+                De um povo heroico, o brado retumbante
+                E o Sol da liberdade, em raios fúlgidos
+                Brilhou no céu da pátria nesse instante");
+
+echo "<br>";
+$brasil->bandeira('<img src="img/brasil.webp" alt="brasil">');
+
+
+echo "<hr>";
+
+
+$cruzeiro = new Clubes("Cruzeiro", 5, "Minas Gerais", "Artur Jorge");
+$cruzeiro->divisao = '1º divisão';
+$cruzeiro->rebaixado = 'Siim, uma vez';
+
+echo "Clube: " . $cruzeiro->nome;
+echo "<br>";
+echo " titulos: " . $cruzeiro->titulos;
+echo "<br>";
+echo " local: " . $cruzeiro->local;
+echo "<br>";
+echo " técnico: " . $cruzeiro->tecnico;
+echo "<br>";
+"Hino: " .$cruzeiro->hino("Existe um grande clube na cidade
+                Que mora dentro do meu coração
+                Eu vivo cheio de vaidade
+                Pois na realidade é um grande campeão");
+
+                
+echo "<br>";
+$brasil->bandeira('<img src="img/cruzeiro.webp" alt="brasil">');
+echo "<hr>";
---
+++ PHP/ORIENTADOOBJETOS/encapsulamento/index.php
@@ -0,0 +1,105 @@
+<style>
+    body {
+        background-color: #103e69ff;
+        color: #fff;
+        font-weight: bold;
+        font-size: 24px;
+    }
+</style>
+
+<?php
+
+class Pessoa
+{
+    public $nome;
+    private $idade;
+    protected $clube;
+    private  $senha = 123;
+
+    public function getNome($nome)
+    {
+        return $this->nome = $nome;
+    }
+
+    public function getIdade($idade)
+    {
+        return $this->idade = $idade;
+    }
+
+    public function getClube($clube)
+    {
+        return $this->clube = $clube;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+
+    public function __get($valor)
+    {
+        return $this->$valor;
+    }
+
+    protected function idade()
+    {
+        return $this->idade;
+    }
+
+    private function clube()
+    {
+        return $this->clube;
+    }
+
+    public function senha()
+    {
+        return $this->senha;
+    }
+
+    public function apresentaPessoa()
+    {
+        $num = rand(1, 9);
+
+        if ($num > 4 && $num < 8) {
+            return "Nome: " . $this->getNome('Sarah') . "<br>" .  "Idade: " . $this->idade();
+        } else {
+            return "Idade: " . $this->getIdade(24) . "<br>" . "Clube: " . $this->clube();
+        }
+    }
+
+    public function verifica()
+    {
+        $nome = $this->nome;
+
+        if (strlen($nome) >= 6) {
+            return "Sua senha é: " . $this->senha();
+        } else {
+            return "Acesso inválido";
+        }
+    }
+}
+
+
+$p1 = new Pessoa;
+
+// echo $p1->getNome("João");
+// echo "<br>";
+// echo $p1->getIdade(15);
+// echo "<br>";
+// echo $p1->getClube("Brasil");
+
+$p1->__set("nome", "Joanas");
+$p1->__set("idade", 20);
+$p1->__set("clube", "Santos");
+
+echo "Nome: " . $p1->__get("nome");
+echo "<br>";
+echo "Idade: " . $p1->__get("idade");
+echo "<br>";
+echo "Clube: " . $p1->__get("clube");
+
+echo "<hr>";
+
+echo $p1->apresentaPessoa();
+echo "<br>";
+echo $p1->verifica();
