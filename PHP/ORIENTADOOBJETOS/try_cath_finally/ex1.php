73afaadc61efc7604af7a03604526e924ebd6899
Andelson <andelsonascimento@gmail.com>
2026-04-22 17:20:45 -0300

    fim

--- PHP/ORIENTADOOBJETOS/try_cath_finally/ex1.php
+++ PHP/ORIENTADOOBJETOS/try_cath_finally/ex1.php
@@ -5,7 +5,10 @@ function dividir($n1, $n2)
 
     try { // primeiro tenta executar o try
         if ($n2 === 0) {
-            throw new Exception("Não pode dividir por zero! <br>"); // cria uma exceção
+            throw new Exception("Não pode dividir por $n2! <br>"); // cria uma exceção
+            // $resul = $n1 / $n2;
+
+            // echo $resul;
         }
 
         $resul = $n1 / $n2;
@@ -15,11 +18,13 @@ function dividir($n1, $n2)
         echo "Error: " . $e->getMessage();
     } finally { // esse é sempre executado
         echo "<br> Finalizado <br>";
+        echo "################";
+        echo "<br> <br>";
     }
 }
 
-// nesse caso aqui não vai cair no catch pois tem um if no try que pega o erro
-
+// nesse caso aqui não vai cair no catch pois tem um if no try que pega o erro (cria uma exceção)
 dividir(10, 0);
+
 dividir(186, 15);
 dividir(90, 7);
